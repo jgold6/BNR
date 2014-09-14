a35683b28e18530b9f3018634ba5dc95aa40b721
Jonathan Goldberger <github@johnnygold.com>
2014-09-13 20:15:59 -1000

    End Chapter 1

---
+++ BNR_Android_Book/GeoQuiz/src/com/onobytes/geoquiz/TrueFalse.java
@@ -0,0 +1,13 @@
+package com.onobytes.geoquiz;
+
+public class TrueFalse {
+	private int mQuestion;
+	
+	private boolean mTrueQuestion;
+	
+	public TrueFalse(int question, boolean trueQuestion)
+	{
+		mQuestion = question;
+		mTrueQuestion = trueQuestion;
+	}
+}
