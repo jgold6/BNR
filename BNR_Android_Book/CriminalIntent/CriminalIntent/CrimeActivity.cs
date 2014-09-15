00ba2910aad2eb3dbf536193f351c2d059e133b7
Jonathan Goldberger <github@johnnygold.com>
2014-09-14 21:13:33 -1000

    CriminalIntent Start Chapter 7

---
+++ BNR_Android_Book/CriminalIntent/CriminalIntent/CrimeActivity.cs
@@ -0,0 +1,55 @@
+﻿#region - using statements
+using System;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Support.V4.App;
+
+
+#endregion
+
+namespace CriminalIntent
+{
+	[Activity(Label = "@string/app_name", MainLauncher = true, Icon = "@drawable/ic_launcher", Theme="@style/AppTheme")]
+    public class CrimeActivity : FragmentActivity
+    {
+		#region - member variables
+		#endregion
+
+		#region - Lifecycle
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+			SetContentView(Resource.Layout.activity_crime);
+
+
+        }
+
+		// To be used later
+//		public override bool OnCreateOptionsMenu(IMenu menu) {
+//			// Inflate the menu; this adds items to the action bar if it is present.
+//			MenuInflater.Inflate(Resource.Menu.menu_quiz, menu);
+//			return true;
+//		}
+//
+//		public override bool OnOptionsItemSelected(IMenuItem item) {
+//			// Handle action bar item clicks here. The action bar will
+//			// automatically handle clicks on the Home/Up button, so long
+//			// as you specify a parent activity in AndroidManifest.xml.
+//			int id = item.ItemId;
+//			if (id == Resource.Id.menu_settings) {
+//				Console.WriteLine("Settings called");
+//				return true;
+//			}
+//			return base.OnOptionsItemSelected(item);
+//		}
+		#endregion
+    }
+}
+
+
