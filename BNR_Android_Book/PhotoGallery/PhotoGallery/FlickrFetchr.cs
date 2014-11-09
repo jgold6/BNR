53c04268fe284a2dd1c7737a3d32847d48acc40d
Jonathan Goldberger <github@johnnygold.com>
2014-11-08 16:13:24 -1000

    * FlickrFetchr.cs: Preload images
    * PhotoGallery.csproj: Less noise in the application output
    
    * PhotoGalleryFragment.cs: Preload images in ScrollStateChanged
    and in SetupAdapter for initial preload.

--- BNR_Android_Book/PhotoGallery/PhotoGallery/FlickrFetchr.cs
+++ BNR_Android_Book/PhotoGallery/PhotoGallery/FlickrFetchr.cs
@@ -25,6 +25,7 @@ namespace PhotoGallery
 		#endregion
 
 		async Task<byte[]> GetUrlBytesAsync(string url) {
+//			Console.WriteLine("[{0}] Start GetUrlBytesAsync: {1}", TAG, DateTime.Now.ToLongTimeString());
 			byte[] result = null;
 			try {
 				using (HttpClient httpCLient = new HttpClient())
@@ -37,37 +38,36 @@ namespace PhotoGallery
 			catch (Exception ex) {
 				Console.WriteLine("Excpetion: {0}", ex.Message);
 			}
+//			Console.WriteLine("[{0}] End GetUrlBytesAsync: {1}", TAG, DateTime.Now.ToLongTimeString());
 			return result;
 		}
 
 		public async Task<string> GetUrlAsync(string url)
 		{
+//			Console.WriteLine("[{0}] Start GetUrlAsync: {1}", TAG, DateTime.Now.ToLongTimeString());
 			string result = "";
 			byte[] bytes = await GetUrlBytesAsync(url).ConfigureAwait(false);
 			foreach (byte bit in bytes) {
 				result += Convert.ToChar(bit);
 			}
+//			Console.WriteLine("[{0}] End GetUrlAsync: {1}", TAG, DateTime.Now.ToLongTimeString());
 			return result;
 		}
 
-		// TODO: Limit caching to 50 images.
-		// TODO: Preload 50 images.
-
-		public async Task<Bitmap> GetImageBitmapAsync(string url)
+		public async Task<Bitmap> GetImageBitmapAsync(string url, int position)
 		{
+//			Console.WriteLine("[{0}] Start GetImageBitmapAsync: {1}", TAG, DateTime.Now.ToLongTimeString());
 			Bitmap bitmap = null;
-			string[] split = url.Split(new char[]{'/'});
-			string filename = split[split.Length-1];
-			string path = System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Personal), filename + ".bytes");
+			string path = GetPathFromUrl(url);
 			// Check for cached images on filesystem. Filename will be the last part of the url+.bytes.
 			if (File.Exists(path)) {
 				try {
 					byte[] bitmapBytes = File.ReadAllBytes(path);
 					bitmap = await BitmapFactory.DecodeByteArrayAsync(bitmapBytes, 0, bitmapBytes.Length);
-					Console.WriteLine("[{0}] Bitmap created from cache", TAG);
+					Console.WriteLine("[{0}] Bitmap created from cache: Position: {1}", TAG, position);
 				}
 				catch (Exception ex) {
-					Console.WriteLine("[{0}] Bitmap creation from cache failed: {1}", TAG, ex.Message);
+					Console.WriteLine("[{0}] Bitmap creation from cache failed: {1}, Position: {2}", TAG, ex.Message, position);
 				}
 			}
 			// If not, download and cache it.
@@ -76,17 +76,19 @@ namespace PhotoGallery
 					byte[] bitmapBytes = await GetUrlBytesAsync(url);
 					bitmap = await BitmapFactory.DecodeByteArrayAsync(bitmapBytes, 0, bitmapBytes.Length);
 					File.WriteAllBytes(path, bitmapBytes);
-					Console.WriteLine("[{0}] Bitmap created from url", TAG);
+					Console.WriteLine("[{0}] Bitmap created from url: Position: {1}", TAG, position);
 				}
 				catch (Exception ex) {
-					Console.WriteLine("[{0}] Bitmap creation from url failed: {1}", TAG, ex.Message);
+					Console.WriteLine("[{0}] Bitmap creation from url failed: {1}}, Position: {2}", TAG, ex.Message, position);
 				}
 			}
+//			Console.WriteLine("[{0}] End GetImageBitmapAsync: {1}", TAG, DateTime.Now.ToLongTimeString());
 			return bitmap;
 		}
 
 		public async Task<List<GalleryItem>> Fetchitems(string pageNum = "1")
 		{
+			Console.WriteLine("[{0}] Start Fetchitems: {1}", TAG, DateTime.Now.ToLongTimeString());
 			List<GalleryItem> items = new List<GalleryItem>();
 
 			try {
@@ -100,13 +102,15 @@ namespace PhotoGallery
 				builder.Query = query.ToString();
 
 				string url = builder.ToString();
+//				Console.WriteLine("[{0}] Fetchitems Start GetUrlAsync: {1}", TAG, DateTime.Now.ToLongTimeString());
 				string xmlString = await GetUrlAsync(url).ConfigureAwait(false);
-
+//				Console.WriteLine("[{0}] Fetchitems End GetUrlAsync: {1}", TAG, DateTime.Now.ToLongTimeString());
 				//Console.WriteLine("[{0}] Received xml from Url: {1}\n{2}", TAG, url, xmlString);
 				//https://api.flickr.com:443/services/rest/?method=flickr.photos.getRecent&api_key=ea248fbeac480b7c14fce5cece516ef0&extras=url_s
 
 				XmlSerializer serializer = new XmlSerializer(typeof(rsp));
 
+//				Console.WriteLine("[{0}] Fetchitems Start Deserialzer: {1}", TAG, DateTime.Now.ToLongTimeString());
 				using (var reader = new StringReader(xmlString)) {
 					var rsp = (rsp)serializer.Deserialize(reader);
 					var photos = rsp.photos.photo;
@@ -120,6 +124,7 @@ namespace PhotoGallery
 						items.Add(item);
 					}
 				}
+//				Console.WriteLine("[{0}] Fetchitems End Deserialzer: {1}", TAG, DateTime.Now.ToLongTimeString());
 			}
 			catch (IOException ioex) {
 				Console.WriteLine("[{0}] Failed to fetch items: {1}", TAG, ioex.Message);
@@ -158,8 +163,86 @@ namespace PhotoGallery
 //			catch (Exception ex) {
 //				Console.WriteLine("[{0}] Failed to fetch items: {1}", TAG, ex.Message);
 //			}
-
+			Console.WriteLine("[{0}] End Fetchitems: {1}", TAG, DateTime.Now.ToLongTimeString());
 			return items;
+		}
+
+		public async Task PreloadImages(int pos1, int pos2, List<GalleryItem> items)
+		{
+			Console.WriteLine("[{0}] Start PreloadImages: {1}", TAG, DateTime.Now.ToLongTimeString());
+//			Console.WriteLine("[{0}] PreloadImages Start Load photos: {1}", TAG, DateTime.Now.ToLongTimeString());
+			// Preload visible and 10 before and after photos
+//			for  (int i = (pos1 - 10 >= 0 ? pos1 - 10 : 0); i < (pos2 + 10 < items.Count ? pos2 + 10 : items.Count); i++) {
+//				string path = GetPathFromUrl(items[i].Url);
+//				if (!File.Exists(path)) {
+//					try {
+//						byte[] bitmapBytes = await GetUrlBytesAsync(items[i].Url).ConfigureAwait(false);
+//						File.WriteAllBytes(path, bitmapBytes);
+//						Console.WriteLine("[{0}] Preload Bitmap created from url: Position: {1}", TAG, i);
+//					}
+//					catch (Exception ex) {
+//						Console.WriteLine("[{0}] Preload Bitmap creation from url failed: {1}, Position: {2}", TAG, ex.Message, i);
+//					}
+//				}
+//			}
+
+			// Preload 10 before displated photos
+			for  (int i = (pos1 - 10 >= 0 ? pos1 - 10 : 0); i < pos1; i++) {
+				string path = GetPathFromUrl(items[i].Url);
+				if (!File.Exists(path)) {
+					try {
+						byte[] bitmapBytes = await GetUrlBytesAsync(items[i].Url).ConfigureAwait(false);
+						File.WriteAllBytes(path, bitmapBytes);
+						Console.WriteLine("[{0}] Preload Bitmap created from url: Position: {1}", TAG, i);
+					}
+					catch (Exception ex) {
+						Console.WriteLine("[{0}] Preload Bitmap creation from url failed: {1}, Position: {2}", TAG, ex.Message, i);
+					}
+				}
+			}
+			// Preload 10 past displayed photos
+			for  (int i = pos2; i < (pos2 + 10 < items.Count ? pos2 + 10 : items.Count); i++) {
+				string path = GetPathFromUrl(items[i].Url);
+				if (!File.Exists(path)) {
+					try {
+						byte[] bitmapBytes = await GetUrlBytesAsync(items[i].Url).ConfigureAwait(false);
+						File.WriteAllBytes(path, bitmapBytes);
+						Console.WriteLine("[{0}] Preload Bitmap created from url: Position: {1}", TAG, i);
+					}
+					catch (Exception ex) {
+						Console.WriteLine("[{0}] Preload Bitmap creation from url failed: {1}, Position: {2}", TAG, ex.Message, i);
+					}
+				}
+			}
+//			Console.WriteLine("[{0}] PreloadImages End Load photos: {1}", TAG, DateTime.Now.ToLongTimeString());
+//			Console.WriteLine("[{0}] PreloadImages Start Delete photos: {1}", TAG, DateTime.Now.ToLongTimeString());
+			// Delete out of range photos
+			for (int i = 0; i < pos1 -10; i++) {
+				string path = GetPathFromUrl(items[i].Url);
+				if (File.Exists(path)) {
+					File.Delete(path);
+					Console.WriteLine("[{0}] File Deleted: Position: {1}", TAG, i);
+				}
+			}
+			if (pos2 + 10 < items.Count) {
+				for (int i = pos2 + 10; i < items.Count; i++) {
+					string path = GetPathFromUrl(items[i].Url);
+					if (File.Exists(path)) {
+						File.Delete(path);
+						Console.WriteLine("[{0}] File Deleted:  Position: {1}", TAG, i);
+					}
+				}
+			}
+//			Console.WriteLine("[{0}] PreloadImages End Delete photos: {1}", TAG, DateTime.Now.ToLongTimeString());
+			Console.WriteLine("[{0}] End PreloadImages: {1}", TAG, DateTime.Now.ToLongTimeString());
+		}
+
+		string GetPathFromUrl(string url)
+		{
+			string[] split = url.Split(new char[]{'/'});
+			string filename = split[split.Length-1];
+			string path = System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Personal), filename + ".bytes");
+			return path;
 		}
     }
 }
--- BNR_Android_Book/PhotoGallery/PhotoGallery/PhotoGalleryFragment.cs
+++ BNR_Android_Book/PhotoGallery/PhotoGallery/PhotoGalleryFragment.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using System.Collections.Generic;
 using System.Linq;
 using Android.Graphics;
+using Android.Util;
 
 namespace PhotoGallery
 {
@@ -20,20 +21,34 @@ namespace PhotoGallery
 
 		public override async void OnCreate(Android.OS.Bundle savedInstanceState)
 		{
+//			Console.WriteLine("[{0}] OnCreate Called: {1}", TAG, DateTime.Now.ToLongTimeString());
 			base.OnCreate(savedInstanceState);
 			RetainInstance = true;
 
 			currentPage = 1;
 			endReached = false;
-			galleryItems = await new FlickrFetchr().Fetchitems(currentPage.ToString());
+			FlickrFetchr fetchr = new FlickrFetchr();
+
+			ProgressDialog pg = new ProgressDialog(Activity);
+			pg.SetMessage("This may take a minute or two");
+			pg.SetTitle("Loading Images");
+			pg.SetCancelable(false);
+			pg.Show();
+
+			galleryItems = await fetchr.Fetchitems(currentPage.ToString());
 //			foreach (GalleryItem item in galleryItems) {
 //				Console.WriteLine("[{0}]\nPhoto Id: {1}\nCaption: {2}\nUrl: {3}", TAG, item.Id, item.Caption, item.Url);
 //			}
+
+			pg.Dismiss();
+
 			SetupAdapter();
+//			Console.WriteLine("[{0}] OnCreate Done: {1}", TAG, DateTime.Now.ToLongTimeString());
 		}
 
 		public override Android.Views.View OnCreateView(Android.Views.LayoutInflater inflater, Android.Views.ViewGroup container, Android.OS.Bundle savedInstanceState)
 		{
+//			Console.WriteLine("[{0}] OnCreateView Called: {1}", TAG, DateTime.Now.ToLongTimeString());
 			View v = inflater.Inflate(Resource.Layout.fragment_photo_gallery, container, false);
 
 			mGridView = v.FindViewById<GridView>(Resource.Id.gridView);
@@ -41,7 +56,7 @@ namespace PhotoGallery
 			mGridView.Scroll += async (object sender, AbsListView.ScrollEventArgs e) => {
 				if (e.FirstVisibleItem + e.VisibleItemCount == e.TotalItemCount && !endReached && e.TotalItemCount > 0) {
 					endReached = true;
-					Console.WriteLine("[{0}] Scroll Ended", TAG);
+//					Console.WriteLine("[{0}] Scroll Ended", TAG);
 					currentPage++;
 					List<GalleryItem> newItems = await new FlickrFetchr().Fetchitems(currentPage.ToString());
 					galleryItems = galleryItems.Concat(newItems).ToList();
@@ -51,9 +66,18 @@ namespace PhotoGallery
 					endReached = false;
 				}
 			};
-			 
-			SetupAdapter();
 
+			mGridView.ScrollStateChanged += (object sender, AbsListView.ScrollStateChangedEventArgs e) => {
+				var adapter = (ArrayAdapter)mGridView.Adapter;
+				if (e.ScrollState == ScrollState.Idle) {
+					Task.Run(async () => {
+						await new FlickrFetchr().PreloadImages(mGridView.FirstVisiblePosition, mGridView.LastVisiblePosition, galleryItems);
+					});
+				}
+			};
+
+			SetupAdapter();
+//			Console.WriteLine("[{0}] OnCreateView Done: {1}", TAG, DateTime.Now.ToLongTimeString());
 			return v;
 		}
 
@@ -61,7 +85,23 @@ namespace PhotoGallery
 			if (Activity == null || mGridView == null) return;
 
 			if (galleryItems != null) {
-				mGridView.Adapter = new GalleryItemAdapter(Activity, galleryItems);
+				var adapter = new GalleryItemAdapter(Activity, galleryItems);
+				mGridView.Adapter = adapter;
+				Task.Run(async () => {
+					Display display = Activity.WindowManager.DefaultDisplay;
+					DisplayMetrics outMetrics = new DisplayMetrics();
+					display.GetMetrics(outMetrics);
+
+					float density = Activity.Resources.DisplayMetrics.Density;
+					var height = outMetrics.HeightPixels / density;
+					var width = outMetrics.WidthPixels / density;
+
+					int itemsPerRow = (int)Math.Round(width/120);
+					int numRows = (int)Math.Round(height/120);
+					int numItems = itemsPerRow * numRows;
+
+					await new FlickrFetchr().PreloadImages(0, numItems, galleryItems).ConfigureAwait(false);
+				});
 			}
 			else {
 				mGridView.Adapter = null;
@@ -81,17 +121,21 @@ namespace PhotoGallery
 
 		public override View GetView(int position, View convertView, ViewGroup parent)
 		{
-			View view = convertView;
-			if (view == null) {
-				view = context.LayoutInflater.Inflate(Resource.Layout.gallery_item, parent, false);
-			}
+			// Recycle
+//			View view = convertView;
+//			if (view == null) {
+//				view = context.LayoutInflater.Inflate(Resource.Layout.gallery_item, parent, false);
+//			}
+
+			// Don't recycle
+			View view = context.LayoutInflater.Inflate(Resource.Layout.gallery_item, parent, false);
 
 			ImageView imageView = view.FindViewById<ImageView>(Resource.Id.gallery_item_imageView);
 			imageView.SetImageResource(Resource.Drawable.face_icon);
-			
+
 			GalleryItem item = GetItem(position);
 			Task.Run(async () => {
-				Bitmap image = await new FlickrFetchr().GetImageBitmapAsync(item.Url).ConfigureAwait(false);
+				Bitmap image = await new FlickrFetchr().GetImageBitmapAsync(item.Url, position).ConfigureAwait(false);
 				context.RunOnUiThread(() => {
 					imageView.SetImageBitmap(image);
 				});
@@ -99,6 +143,8 @@ namespace PhotoGallery
 
 			return view;
 		}
+
+
 	}
 }
 
