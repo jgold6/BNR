0eb1906185e9f06e4d680af9f782aa3e6c33f446
Jonathan Goldberger <github@johnnygold.com>
2014-11-15 22:05:17 -1000

    Page 529

---
+++ BNR_Android_Book/RunTracker/RunTracker/RunActivity.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace RunTracker
+{
+	[Activity(Label = "RunTracker", MainLauncher = true, Icon = "@drawable/icon", Theme="@style/AppTheme")]
+	public class RunActivity : SingleFragmentActivity
+    {
+		protected override Fragment CreateFragment()
+		{
+			return new RunFragment();
+		}
+    }
+}
+
+
---
+++ BNR_Android_Book/RunTracker/RunTracker/RunFragment.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Android.App;
+using Android.Widget;
+using Android.Views;
+
+namespace RunTracker
+{
+    public class RunFragment : Fragment
+    {
+		Button mStartButton, mStopButton;
+		TextView mStartedTextView, mLatitudeTextView, mLongitudeTextView, mAltitudeTextView, mDurationTextView;
+
+		public override void OnCreate(Android.OS.Bundle savedInstanceState)
+		{
+			base.OnCreate(savedInstanceState);
+			RetainInstance = true;
+		}
+
+		public override Android.Views.View OnCreateView(Android.Views.LayoutInflater inflater, Android.Views.ViewGroup container, Android.OS.Bundle savedInstanceState)
+		{
+			View view = inflater.Inflate(Resource.Layout.fragment_run, container, false);
+
+			mStartedTextView = view.FindViewById<TextView>(Resource.Id.run_startedTextView);
+			mLatitudeTextView = view.FindViewById<TextView>(Resource.Id.run_latitudeTextView);
+			mLongitudeTextView = view.FindViewById<TextView>(Resource.Id.run_longitudeTextView);
+			mAltitudeTextView = view.FindViewById<TextView>(Resource.Id.run_altitudeTextView);
+			mDurationTextView = view.FindViewById<TextView>(Resource.Id.run_durationTextView);
+
+			mStartButton = view.FindViewById<Button>(Resource.Id.run_startButton);
+			mStopButton = view.FindViewById<Button>(Resource.Id.run_stopButton);
+
+			return view;
+		}
+    }
+}
+
