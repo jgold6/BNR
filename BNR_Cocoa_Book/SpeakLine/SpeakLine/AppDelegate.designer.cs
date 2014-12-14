3d06a804ceba0e60fbd19e20a4e655ee9b731756
Jonathan Goldberger <github@johnnygold.com>
2014-12-14 13:50:23 -1000

    Added SpeakLine

---
+++ BNR_Cocoa_Book/SpeakLine/SpeakLine/AppDelegate.designer.cs
@@ -0,0 +1,10 @@
+﻿
+namespace SpeakLine
+{
+    // Should subclass MonoMac.AppKit.NSResponder
+    [MonoMac.Foundation.Register("AppDelegate")]
+    public partial class AppDelegate
+    {
+    }
+}
+
