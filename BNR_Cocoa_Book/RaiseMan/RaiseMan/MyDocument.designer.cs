122a1bfa2d7c20f639db59af0e24061757f70485
Jonathan Goldberger <github@johnnygold.com>
2014-12-23 14:56:04 -1000

    Start Chapter 8

---
+++ BNR_Cocoa_Book/RaiseMan/RaiseMan/MyDocument.designer.cs
@@ -0,0 +1,12 @@
+﻿
+namespace RaiseMan
+{
+    
+    // Should subclass MonoMac.AppKit.NSDocument
+    [MonoMac.Foundation.Register("MyDocument")]
+    public partial class MyDocument
+    {
+    }
+}
+
+
