19df3f35a5b96f133e6fb73ffb2cc67199262859
Jonathan Goldberger <github@johnnygold.com>
2014-12-27 19:11:38 -1000

    Chapter 11 Xcode CoreData

---
+++ BNR_Cocoa_Book/Xcode Versions/CarLotXC/CarLotXC/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  CarLotXC
+//
+//  Created by Jonathan Goldberger on 12/27/14.
+//  Copyright (c) 2014 Jonathan Goldberger. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
