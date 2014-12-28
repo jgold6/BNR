56d45f7456950606894368517cd383dcb87f590c
Jonathan Goldberger <github@johnnygold.com>
2014-12-27 20:07:51 -1000

    Xcode CoreData No Code finished?

---
+++ BNR_Cocoa_Book/Xcode Versions/CarLot/CarLot/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CarLot
+//
+//  Created by Jonathan Goldberger on 12/27/14.
+//  Copyright (c) 2014 Jonathan Goldberger. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ BNR_Cocoa_Book/Xcode Versions/CarLot/CarLot/Document.h
@@ -0,0 +1,12 @@
+//
+//  Document.h
+//  CarLot
+//
+//  Created by Jonathan Goldberger on 12/27/14.
+//  Copyright (c) 2014 Jonathan Goldberger. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface Document : NSPersistentDocument
+@end
