56b020052510226618603ece9a0907e9cd5e4ac7
Jonathan Goldberger <github@johnnygold.com>
2015-01-26 01:08:55 -1000

    Why did this not commit before?

---
+++ BNR_Cocoa_Book/DrawingOvals/DrawingOvals/Oval.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using CoreGraphics;
+using Foundation;
+
+namespace DrawingOvals
+{
+    public class Oval : NSObject
+    {
+		[Export("startPoint")]
+		public CGPoint StartPoint {get; set;}
+		[Export("endPoint")]
+		public CGPoint Endpoint {get; set;}
+
+		public CGRect Rect {
+			get {
+				return new CGRect(StartPoint.X, StartPoint.Y, Endpoint.X - StartPoint.X, Endpoint.Y - StartPoint.Y);
+			}
+		}
+
+		public Oval() : base()
+		{
+
+		}
+
+		[Export("initWithCoder:")]
+		public Oval(NSCoder decoder)
+		{
+			StartPoint = new CGPoint(decoder.DecodeFloat("startPointX"), decoder.DecodeFloat("startPointY"));
+
+			Endpoint = new CGPoint(decoder.DecodeFloat("endPointX"), decoder.DecodeFloat("endPointY"));
+		}
+
+		[Export("encodeWithCoder:")]
+		public void EncodeTo(NSCoder coder)
+		{
+			coder.Encode(StartPoint.X, "startPointX");
+			coder.Encode(StartPoint.Y, "startPointY");
+			coder.Encode(Endpoint.X, "endPointX");
+			coder.Encode(Endpoint.Y, "endPointY");
+		}
+    }
+}
+
