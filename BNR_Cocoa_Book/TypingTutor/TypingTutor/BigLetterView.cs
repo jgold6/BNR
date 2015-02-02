516c2c7189f34b348314900490e0a4565c25fca6
Jonathan Goldberger <github@johnnygold.com>
2015-02-01 19:29:34 -1000

    Chapter 20 pre-challenges

--- BNR_Cocoa_Book/TypingTutor/TypingTutor/BigLetterView.cs
+++ BNR_Cocoa_Book/TypingTutor/TypingTutor/BigLetterView.cs
@@ -10,6 +10,8 @@ namespace TypingTutor
     public partial class BigLetterView : AppKit.NSView
     {
 		#region - Member variables and Properties
+		NSMutableDictionary mAttributes;
+
 		NSColor mBgColor;
 		public NSColor BgColor {
 			get {
@@ -53,6 +55,7 @@ namespace TypingTutor
         void Initialize()
         {
 			Console.WriteLine("Initializing BigLetterView");
+			PrepareAttributes();
 			mBgColor = NSColor.Yellow;
 			mLetter = " ";
         }
@@ -148,12 +151,48 @@ namespace TypingTutor
 //				NSBezierPath.StrokeRect(bounds);	
 //			}
 
-			NSString drawLetter = new NSString(mLetter);
-			NSMutableDictionary attributes = new NSMutableDictionary();
-			attributes.Add(NSAttributedString.FontAttributeName, NSFont.FromFontName("Helvetica", 228.0f));
-			var textSize = drawLetter.StringSize(attributes);
+			DrawStringCenteredInRectangle(mLetter, bounds);
+		}
+		#endregion
 
-			drawLetter.DrawString(new CGPoint(bounds.Width/2-textSize.Width/2, bounds.Height/2-textSize.Height/2), attributes);
+		#region - Actions
+		[Action ("savePDF:")]
+		public void SavePDF (Foundation.NSObject sender)
+		{
+			NSSavePanel panel = NSSavePanel.SavePanel;
+			panel.AllowedFileTypes = new string[]{"pdf"};
+			panel.BeginSheet(this.Window, (result) =>  {
+				if (result == 1) {
+					CGRect r = this.Bounds;
+					NSData data = this.DataWithPdfInsideRect(r);
+					NSError error = null;
+					bool successful = data.Save(panel.Url, false, out error);
+					if (!successful) {
+						NSAlert a = NSAlert.WithError(error);
+						a.RunModal();
+					}
+				}
+				panel = null;
+			});
+		}
+		#endregion
+
+		#region - Methods
+		void PrepareAttributes()
+		{
+			mAttributes = new NSMutableDictionary();
+			mAttributes.Add(NSAttributedString.FontAttributeName, NSFont.UserFontOfSize(75.0f));
+			mAttributes.Add(NSAttributedString.ForegroundColorAttributeName, NSColor.Red);
+		}
+
+		void DrawStringCenteredInRectangle(string str, CGRect rect)
+		{
+			NSString drawLetter = new NSString(str);
+			CGSize strSize = drawLetter.StringSize(mAttributes);
+			CGPoint strOrigin = new CGPoint();
+			strOrigin.X = rect.Location.X + (rect.Size.Width - strSize.Width)/2;
+			strOrigin.Y = rect.Location.Y + (rect.Size.Height - strSize.Height)/2;
+			drawLetter.DrawString(strOrigin, mAttributes);
 		}
 		#endregion
     }
