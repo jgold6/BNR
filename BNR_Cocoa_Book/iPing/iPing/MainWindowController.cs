068329d52c4089bd5d486eaab2e00c3fb54286cb
Jonathan Goldberger <github@johnnygold.com>
2015-05-04 18:36:46 -1000

    Finished iPing. No enum for string key NSFileHandleNotificationDataItem.

--- BNR_Cocoa_Book/iPing/iPing/MainWindowController.cs
+++ BNR_Cocoa_Book/iPing/iPing/MainWindowController.cs
@@ -2,11 +2,16 @@
 
 using Foundation;
 using AppKit;
+using ObjCRuntime;
 
 namespace iPing
 {
     public partial class MainWindowController : NSWindowController
     {
+		NSTask task;
+		NSPipe pipe;
+
+
         public MainWindowController(IntPtr handle) : base(handle)
         {
         }
@@ -29,5 +34,71 @@ namespace iPing
         {
             get { return (MainWindow)base.Window; }
         }
+
+		partial void startStopPing (Foundation.NSObject sender)
+		{
+			if (task != null) {
+				task.Interrupt();
+			}
+			else {
+				task = new NSTask();
+				task.LaunchPath = "/sbin/ping";
+				string[] args = {"-c10", hostField.StringValue};
+
+				task.Arguments = args;
+
+				// Create a new pipe
+				pipe = new NSPipe();
+				task.StandardOutput = pipe;
+
+				NSFileHandle fh = pipe.ReadHandle;
+
+				NSNotificationCenter nc = NSNotificationCenter.DefaultCenter;
+				nc.RemoveObserver(this);
+				nc.AddObserver(this, new Selector("dataReady:"), NSFileHandle.ReadCompletionNotification, fh);
+				nc.AddObserver(this, new Selector("taskTerminated:"), NSTask.NSTaskDidTerminateNotification, task);
+				task.Launch();
+				outputView.Value = "";
+
+				// Suspect = Obj-C example is [fh readInBackgroundAndNotify] - no arguments
+				fh.ReadInBackground();
+			}
+		}
+
+		void AppendData(NSData d)
+		{
+			NSString s = NSString.FromData(d, NSStringEncoding.UTF8);
+			NSTextStorage ts = outputView.TextStorage;
+			ts.Replace(new NSRange(ts.Length, 0), s);
+		}
+
+		[Export("dataReady:")]
+		void DataReady(NSNotification n)
+		{
+			foreach (NSString str in n.UserInfo.Keys) {
+				Console.WriteLine(str);
+			}
+
+			NSData d = (NSData)n.UserInfo.ValueForKey(new NSString("NSFileHandleNotificationDataItem"));
+
+			Console.WriteLine("Data Ready: {0} bytes", d.Length);
+
+			if (d.Length > 0) {
+				this.AppendData(d);
+			}
+
+			// If the task is running, start reading again
+			if (task != null) {
+				pipe.ReadHandle.ReadInBackground();
+			}
+		}
+
+		[Export("taskTerminated:")]
+		void TaskTerminated(NSNotification n)
+		{
+			Console.WriteLine("Task Terminated");
+			task = null;
+			startButton.State = NSCellStateValue.Off;
+		}
     }
 }
--- BNR_Cocoa_Book/iPing/iPing/MainWindowController.designer.cs
+++ BNR_Cocoa_Book/iPing/iPing/MainWindowController.designer.cs
@@ -1,7 +1,45 @@
-﻿namespace iPing
+// WARNING
+//
+// This file has been generated automatically by Xamarin Studio to store outlets and
+// actions made in the UI designer. If it is removed, they will be lost.
+// Manual changes to this file may not be handled correctly.
+//
+using Foundation;
+using System.CodeDom.Compiler;
+
+namespace iPing
 {
-    [global::Foundation.Register("MainWindowController")]
-    public partial class MainWindowController
-    {
-    }
+	[Register ("MainWindowController")]
+	partial class MainWindowController
+	{
+		[Outlet]
+		AppKit.NSTextField hostField { get; set; }
+
+		[Outlet]
+		AppKit.NSTextView outputView { get; set; }
+
+		[Outlet]
+		AppKit.NSButton startButton { get; set; }
+
+		[Action ("startStopPing:")]
+		partial void startStopPing (Foundation.NSObject sender);
+		
+		void ReleaseDesignerOutlets ()
+		{
+			if (hostField != null) {
+				hostField.Dispose ();
+				hostField = null;
+			}
+
+			if (outputView != null) {
+				outputView.Dispose ();
+				outputView = null;
+			}
+
+			if (startButton != null) {
+				startButton.Dispose ();
+				startButton = null;
+			}
+		}
+	}
 }
